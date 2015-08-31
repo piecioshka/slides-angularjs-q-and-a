8aeaa4c0a71a6a47ac0cf446823a635d7c6baeaf
Piotr Kowalski <piecioshka@gmail.com>
2015-08-31 15:26:59 +0200

    Update gamepad.js and shower.gamepad.js

--- scripts/shower.gamepad.js
+++ scripts/shower.gamepad.js
@@ -1,23 +1,51 @@
+/*global Gamepad, shower */
+
 (function () {
     'use strict';
 
-    Gamepad.on('joystick.left', function (evt) {
-        var dirs = evt.direction;
+    if (!Gamepad) {
+        throw new ReferenceError('gamepad.js is required');
+    }
 
-        if (dirs.left) {
+    if (!shower) {
+        throw new ReferenceError('shower.js is required');
+    }
+
+    // Joysticks
+
+    Gamepad.on('joystick:left', function (direction) {
+        if (direction.left) {
             shower.prev();
-        } else if (dirs.right) {
+        } else if (direction.right) {
             shower.next();
         }
     });
 
-    Gamepad.on('joystick.right', function (evt) {
-        var dirs = evt.direction;
-
-        if (dirs.left) {
+    Gamepad.on('joystick:right', function (direction) {
+        if (direction.left) {
             shower.prev();
-        } else if (dirs.right) {
+        } else if (direction.right) {
             shower.next();
         }
+    });
+
+    // Arrows
+
+    Gamepad.on('arrow:right', function () {
+        shower.next();
+    });
+
+    Gamepad.on('arrow:left', function () {
+        shower.prev();
+    });
+
+    // Shapes
+
+    Gamepad.on('shape:circle', function () {
+        shower.next();
+    });
+
+    Gamepad.on('shape:square', function () {
+        shower.prev();
     });
 }());
